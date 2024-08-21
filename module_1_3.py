794255c664ec0b6b5e1c3d5d49a414907d4abdc7
SVyacheslavV <suslavik@yandex.ru>
2024-08-21 13:42:46 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Вячеслав'
 print('Name:', name)
 age = 35
 print('Age:', age)
-new_age = age + 10
-print('New Age:', new_age)
+age = age + 10
+print('New Age:', age)
 is_student = True
 print('Is student:', is_student)
